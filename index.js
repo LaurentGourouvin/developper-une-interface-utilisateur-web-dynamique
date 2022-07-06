910c1144dda9529e06418c5738f4c89c9f156889
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2022-07-06 23:38:39 +0200

    structure de base du projet ok

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+// import node module
 const express = require('express');
-const app = express();
 
+// import own module
+const router = require('./routes/router');
+
+const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}, http://localhost:${port}`);
